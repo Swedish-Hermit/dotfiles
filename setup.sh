bf76c792204eddc9c55cb4538c7aa5703f7c4ba5
robin <robin@L440.home.robinsmediateam.dev>
2022-11-18 23:47:50 +0100

    updated setup script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-if [ ! -d "~/.ssh" ]; then
   sudo apt-get install zsh git curl -y
   mkdir ~/.ssh;
   echo "" >> ~/.ssh/config
-  echo "Include ~/dotfiles/config" > ~/.ssh/config
+  echo "Include ~/config" > ~/.ssh/config
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
   cp -v ~/dotfiles/.zshrc > ~/.zshrc
   . ~/.zshrc
   echo "done adding aliases, ssh configs"
   exit 0
-fi
 echo "have a nice day!"
