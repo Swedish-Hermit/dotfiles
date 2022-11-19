6e9b2e4d597976155f699bbd919dff7b118d290d
Swedish-Hermit <rmtintranet@gmail.com>
2022-11-19 19:54:47 +0100

    updated a script

--- local-cloud.sh
+++ local-cloud.sh
@@ -4,6 +4,6 @@ sh ~/dotfilespingcheck.sh
 sleep 1;
 cd /mnt/local/cdn
 ###rsync the local repo to offsite
-rsync -ahvzuP . --no-perms --delete --exclude msdn/ --exclude Youtube_Archive/ --exclude Cursed_recordings_with_the_bois/ --exclude sidezeoarchive/ $1 > /mnt/local/cdnbackup.log
+rsync -ahvzuP . --no-perms --delete --exclude msdn/ --exclude Cursed_recordings_with_the_bois/ --exclude sidezeoarchive/ $1 > /mnt/local/cdnbackup.log
 echo "exiting script have a nice day"
 exit 0
