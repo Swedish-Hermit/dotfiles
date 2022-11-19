ea840973d65e4c6ec24b2c1232c2f42304e2470c
robin <robin@L440.home.robinsmediateam.dev>
2022-11-19 19:10:02 +0100

    updated setups script

--- setup.sh
+++ setup.sh
@@ -3,9 +3,8 @@
   mkdir ~/.ssh;
   echo "" >> ~/.ssh/config
   echo "Include ~/config" > ~/.ssh/config
-  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-  cp -v ~/dotfiles/.zshrc > ~/.zshrc
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && cp -v ~/dotfiles/.zshrc > ~/.zshrc
   . ~/.zshrc
   echo "done adding aliases, ssh configs"
-  exit 0
+exit 0
 echo "have a nice day!"
