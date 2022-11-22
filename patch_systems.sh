58a3db7c20d07629e18d3c1e0f6bf6284c790bfb
Swedish-Hermit <rmtintranet@gmail.com>
2022-11-22 10:47:19 +0100

    Updated variable in patch_systems.sh

--- patch_systems.sh
+++ patch_systems.sh
@@ -2,11 +2,10 @@
 sh ~/dotfiles/pingcheck.sh
 set -o errexit
 set -o nounset
-system=$1
 ###ssh systemsboy@rhost.systemsboy.edu 'ls -l'
 if [ "$#" -ne 1 ];
 	then echo -e "\nUsage: sh patch_systems.sh hostname\n";
 	exit 1;
 fi
-ssh -t $system 'sudo apt-get update && sudo apt-get upgrade && sudo apt autoremove'
+ssh -t $1 'sudo apt-get update && sudo apt-get upgrade && sudo apt autoremove'
 
