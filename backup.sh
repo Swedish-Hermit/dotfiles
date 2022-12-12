c49367e7919924490b254ad62c6e04dbe217d2e8
Swedish-Hermit <rmtintranet@gmail.com>
2022-12-12 20:01:18 +0100

    Updated backup script

--- backup.sh
+++ backup.sh
@@ -4,4 +4,4 @@ echo Starting HEL1
 cd /mnt && rclone copy -P files /mnt/HEL1/files --ignore-existing --exclude=/datahoarding/ --exclude=/.recycle/ > ~/HEL1files.log & rclone copy -P users /mnt/HEL1/users --ignore-existing --exclude=/.recycle/ > ~/HEL1users.log
 
 echo Starting FSN1
-cd /mnt && rclone copy -P files /mnt/FSN1/files --ignore-existing --exclude=/datahoarding/ --exclude=/.recycle/ > ~/FSN1files.log & rclone copy -P users /mnt/FSN1/users --ignore-existing --exclude=/.recycle/ > ~/FSN1users.log
+cd /mnt && rclone copy -P /mnt/HEL1/files /mnt/FSN1/files --ignore-existing --exclude=/datahoarding/ --exclude=/.recycle/ > ~/FSN1files.log & rclone copy -P /mnt/HEL1/users /mnt/FSN1/users --ignore-existing --exclude=/.recycle/ > ~/FSN1users.log
