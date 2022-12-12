50737016d34f308491a5a064d9c2d0a3d707472a
Swedish-Hermit <rmtintranet@gmail.com>
2022-12-12 20:19:52 +0100

    Updated backup script with variables

--- backup.sh
+++ backup.sh
@@ -2,12 +2,11 @@
 sh ~/dotfiles/pingcheck.sh
 set -o errexit
 set -o nounset
-PATH=/mnt
 echo "Starting HEL1"
-cd $PATH && rclone copy -P files /mnt/HEL1/files --ignore-existing --exclude=/datahoarding/ --exclude=/.recycle/ > ~/HEL1files.log;
-cd $PATH && rclone copy -P users /mnt/HEL1/users --ignore-existing --exclude=/.recycle/ > ~/HEL1users.log;
+cd $1 && rclone copy -P files /mnt/HEL1/files --ignore-existing --exclude=/datahoarding/ --exclude=/.recycle/ > ~/HEL1files.log;
+cd $1 && rclone copy -P users /mnt/HEL1/users --ignore-existing --exclude=/.recycle/ > ~/HEL1users.log;
 
 echo "Starting FSN1"
-cd $PATH && rclone copy -P /mnt/HEL1/files /mnt/FSN1/files --ignore-existing --transfers 30 --exclude=/datahoarding/ --exclude=/.recycle/ > ~/FSN1files.log; 
-cd $PATH && rclone copy -P /mnt/HEL1/users /mnt/FSN1/users --ignore-existing --transfers 30 --exclude=/.recycle/ > ~/FSN1users.log;
+cd $1 && rclone copy -P /mnt/HEL1/files /mnt/FSN1/files --ignore-existing --transfers 30 --exclude=/datahoarding/ --exclude=/.recycle/ > ~/FSN1files.log; 
+cd $1 && rclone copy -P /mnt/HEL1/users /mnt/FSN1/users --ignore-existing --transfers 30 --exclude=/.recycle/ > ~/FSN1users.log;
 exit 0
