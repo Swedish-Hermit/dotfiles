e740fb0953de89fa9c2e4c09a78f07ee5f327062
Swedish-Hermit <rmtintranet@gmail.com>
2022-12-16 22:56:17 +0100

    update backup.sh

--- backup.sh
+++ backup.sh
@@ -8,11 +8,11 @@ then
    echo "rclone is running exiting..."
 else
 echo "Starting HEL1"
-cd $PATH && rclone copy -P files /mnt/HEL1/files --ignore-existing --exclude=/datahoarding/ --exclude=/.recycle/ > ~/HEL1files.log;
-cd $PATH && rclone copy -P users /mnt/HEL1/users --ignore-existing --exclude=/.recycle/ > ~/HEL1users.log;
+cd $PATH && rclone sync -P files /mnt/HEL1/files --exclude=/datahoarding/ --exclude=/.recycle/ > ~/HEL1files.log;
+cd $PATH && rclone sync -P users /mnt/HEL1/users --exclude=/.recycle/ > ~/HEL1users.log;
 
 echo "Starting FSN1"
-cd $PATH && rclone copy -P /mnt/HEL1/files /mnt/FSN1/files --ignore-existing --transfers 30 --exclude=/datahoarding/ --exclude=/.recycle/ > ~/FSN1files.log;
-cd $PATH && rclone copy -P /mnt/HEL1/users /mnt/FSN1/users --ignore-existing --transfers 30 --exclude=/.recycle/ > ~/FSN1users.log;
+cd $PATH && rclone sync -P /mnt/HEL1/files /mnt/FSN1/files --transfers 30 --exclude=/datahoarding/ --exclude=/.recycle/ > ~/FSN1files.log;
+cd $PATH && rclone sync -P /mnt/HEL1/users /mnt/FSN1/users --transfers 30 --exclude=/.recycle/ > ~/FSN1users.log;
 exit 0
 fi
