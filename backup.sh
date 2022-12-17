f31a610705bbdb77169129c76342ef22d2da4c5b
Swedish-Hermit <rmtintranet@gmail.com>
2022-12-17 18:27:07 +0100

    Updated backup script

--- backup.sh
+++ backup.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 sh ~/dotfiles/pingcheck.sh
 set -o errexit
-set -o nounset
-PATH=/mnt
-if pgrep -x "rclone" >/dev/null
+P=/mnt
+if pgrep -x rclone >/dev/null
 then
-   echo "rclone is running exiting..."
-else
-echo "Starting HEL1"
-cd $PATH && rclone sync -P files /mnt/HEL1/files --exclude=/datahoarding/ --exclude=/.recycle/ > ~/HEL1files.log;
-cd $PATH && rclone sync -P users /mnt/HEL1/users --exclude=/.recycle/ > ~/HEL1users.log;
-
-echo "Starting FSN1"
-cd $PATH && rclone sync -P /mnt/HEL1/files /mnt/FSN1/files --transfers 30 --exclude=/datahoarding/ --exclude=/.recycle/ > ~/FSN1files.log;
-cd $PATH && rclone sync -P /mnt/HEL1/users /mnt/FSN1/users --transfers 30 --exclude=/.recycle/ > ~/FSN1users.log;
-exit 0
+   echo "nope"
+   exit 0
 fi
+   echo "Starting backups"
+   cd $P && rclone sync -vP files rmtbackupdata: --exclude=/datahoarding/cdnbackup --exclude=/.recycle/ > /mnt/dockerdata/Data.log;
+   cd $P && rclone sync -vP users/Robin rmtbackuprobin: --exclude=/.recycle/ > /mnt/dockerdata/Robin.log;
+   cd $P && rclone sync -vP users/Robin rmtbackupjeffrey: --exclude=/.recycle/ > /mnt/dockerdata/jeffrey.log;
+
