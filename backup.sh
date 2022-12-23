8782f00153abf839399c75283053e1aa33504e20
Swedish-Hermit <rmtintranet@gmail.com>
2022-12-23 13:08:10 +0100

    updated backup

--- backup.sh
+++ backup.sh
@@ -9,5 +9,5 @@ fi
    echo "Starting backups"
    cd $1 && rclone sync -vP files rmtbackupdata: --exclude=/datahoarding/cdnbackup --exclude=/.recycle/ > /mnt/dockerdata/Data.log;
    cd $1 && rclone sync -vP users/Robin rmtbackuprobin: --exclude=/.recycle/ > /mnt/dockerdata/Robin.log;
-   cd $1 && rclone sync -vP users/Robin rmtbackupjeffrey: --exclude=/.recycle/ > /mnt/dockerdata/jeffrey.log;
+   cd $1 && rclone sync -vP users/Jeffrey rmtbackupjeffrey: --exclude=/.recycle/ > /mnt/dockerdata/jeffrey.log;
 
