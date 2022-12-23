e90ad8c38d8e43a54d20a44a1bd7f5356ced9d91
Swedish-Hermit <rmtintranet@gmail.com>
2022-12-23 15:18:42 +0100

    updated backup script with check binary

--- backup.sh
+++ backup.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 sh ~/dotfiles/pingcheck.sh
 set -o errexit
+
+if ! command -v rclone > /dev/null; then
+   echo "Error rclone binary not found" >&2
+   exit 1
+fi
+
 if pgrep -x rclone >/dev/null
 then
    echo "nope"
-   exit 0
+   exit 1
 fi
    echo "Starting backups"
    cd $1 && rclone sync -vP files rmtbackupdata: --exclude=/datahoarding/cdnbackup --exclude=/.recycle/ > /mnt/dockerdata/Data.log;
