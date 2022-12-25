f02b85371523616eb87f68d7f4e7285b7603d4ef
Swedish-Hermit <rmtintranet@gmail.com>
2022-12-25 15:04:54 +0100

    updated setup script

--- setup.sh
+++ setup.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 apt-get install zsh git curl -y
 mkdir ~/.ssh;
-echo "" >> ~/.ssh/config
+echo "" >> ~/.ssh/config;
 echo "Include ~/dotfiles/config" > ~/.ssh/config
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended --keep-zshrc
-
 cp -v ~/dotfiles/.zshrc > ~/.zshrc
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended --keep-zshrc
 echo "exec zsh" >> ~/.bashrc
-#. ~/.zshrc
 echo "Please disconnect and connect again to run the new shell!"
 exit 0
