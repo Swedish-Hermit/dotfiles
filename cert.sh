c6ddcfe4166902caf171444e1164795d997cc73f
Swedish-Hermit <rmtintranet@gmail.com>
2023-01-07 15:45:53 +0100

    updated cert

--- cert.sh
+++ cert.sh
@@ -3,6 +3,6 @@ cert='~/dotfiles/certs/RMT-PKI-RTCA_Robin!0027s Media Team Root X1_BASE64.crt'
 sh ~/dotfiles/pingcheck.sh
 set -o errexit
 set -o nounset
-###error if not ran with sudo
+# error if not ran with sudo
 [[ "$(id -u)" == 0 ]] || { echo "Run: sudo !!" ; exit 1 ; }
 cp -v $cert /usr/local/share/ca-certificates/ && update-ca-certificates || { echo "Error :: something went wrong ... exiting" ; exit 1 ; }
