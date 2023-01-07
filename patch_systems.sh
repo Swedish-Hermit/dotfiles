0960265959794895b5b5996380b1f1f30eb69be3
Swedish-Hermit <rmtintranet@gmail.com>
2023-01-07 15:45:01 +0100

    updated patch_systems

--- patch_systems.sh
+++ patch_systems.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+# run pingcheck script"
 sh ~/dotfiles/pingcheck.sh
+# make sure everything is working correctly
 set -o errexit
 set -o nounset
-###ssh systemsboy@rhost.systemsboy.edu 'ls -l'
+# ssh systemsboy@rhost.systemsboy.edu 'ls -l'
 if [ "$#" -ne 1 ];
 	then echo -e "\nUsage: sh patch_systems.sh hostname\n";
 	exit 1;
